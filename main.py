0a4d740260a5bc0014c7502e747a5110e8459f37
DESKTOP-VF1R09E\lakne <laknerdiana@gmail.com>
2023-03-25 20:18:03 +0200

    log file working, added comments

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import time
 def log(message):
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
-    with open('log.txt', 'w') as f:
-        f.write(t": "message)
+    with open('log.txt', 'a') as f:
+        f.write(current_time+": "+message)
+        f.write('\n')
 
 # function for comparing files
 def compareFiles(file1, file2):
@@ -33,18 +34,23 @@ def compareFolder(folder, sync):
             else:
                 return False
         else:
-            return False
+                return False
+    return True
     
-folderPath = 'd:/Projects/folder-sync/folder'
-syncPath = 'd:/Projects/folder-sync/sync'
+folderPath = input("Please input your source file path: ") 
+#'d:/Projects/folder-sync/folder'
+syncPath = input("Please input your sync file path: ") 
+#'d:/Projects/folder-sync/sync'
 
-folderFiles = os.listdir('d:/Projects/folder-sync/folder')
-syncFiles = os.listdir('d:/Projects/folder-sync/sync')
+folderFiles = os.listdir(folderPath)
+syncFiles = os.listdir(syncPath)
+
+log("Start")
 
 while True:
-
-    if compareFolders(folderPath, syncPath):
-        print("File up to date")
+    if compareFolder(folderPath, syncPath):
+        log("Folder up to date")
+        print("Folder up to date")
         time.sleep(120)
         continue
 
@@ -52,19 +58,29 @@ while True:
         if file in syncFiles:
             #check if our files are the same
             if compareFiles(folderPath+'/'+file, syncPath+'/'+file):
+                #if file is the same, write to log and console
+                log(file+" is up to date")
                 print(file+" is up to date")
             else:
+                #if not the same, remove file and recopy with correct version
                 os.remove(syncPath+'/'+file)
                 shutil.copy(folderPath+'/'+file, syncPath) #or os.system("cp " +folderPath+'/'+file+' '+backup), but not on windows
+                log(file+" was updated")
                 print(file+" was updated")
         else:
+            #if file not found in sync folder, copy it
             shutil.copy(folderPath+'/'+file, syncPath)
+            log(file+" was copied")
             print(file+" was copied")
 
     for file in syncFiles:
         if file not in folderFiles:
+            #if file from sync folder not in src folder, remove it
             os.remove(syncPath+'/'+file)
+            log(file+" was removed")
             print(file+" was removed")
-
+    
+    print("Folder up to date")
+    log("Folder up to date")
     time.sleep(120)
    
